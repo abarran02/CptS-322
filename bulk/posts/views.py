d46d01726e2bcaadb5e52426de0dd803610e4dbd
Alec Barran <alexanderbarran@gmail.com>
2023-03-23 16:47:05 -0700

    enable user login

--- bulk/posts/views.py
+++ bulk/posts/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the posts index.")
+    return HttpResponse("Hello, world. You're at the Posts index.")
