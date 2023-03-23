05a96769799c6a2ee022795e3994ed03b4107167
Alec Barran <alexanderbarran@gmail.com>
2023-03-23 16:47:05 -0700

    enable user login

--- bulk/bulk/settings.py
+++ bulk/bulk/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'bulk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
