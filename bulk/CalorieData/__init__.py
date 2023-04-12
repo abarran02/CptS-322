ce961dfeb50d93653aca1f0a106ad2b927114115
Alec Barran <alexanderbarran@gmail.com>
2023-04-12 11:35:00 -0700

    relative paths for CalorieData

--- bulk/CalorieData/__init__.py
+++ bulk/CalorieData/__init__.py
@@ -1,15 +1,18 @@
+import os
 from json import loads
 
 class FoodData:
     """
-    Dataclass to ingest foods.json and provide the following data members:
+    Dataclass to ingest `foods.json` and provide the following data members:
     * all_data: raw JSON ingest as a list of dictionaries
     * calorie_lookup: dict of format { restaurant_name:{food_name:calories, ...}, ... } for
     fast calorie lookup given exact restaurant and food names
     * restaurants: list of all valid restaurants
     """
     def __init__(self):
-        with open('bulk/CalorieData/foods.json', 'r') as food_file:
+        # get this file's directory to find foods.json
+        json_path = os.path.join(os.path.dirname(__file__), 'foods.json')
+        with open(json_path, 'r') as food_file:
             self.all_data = loads(food_file.read())
 
         # create nested dictionary of format above for easy calorie lookup
@@ -31,13 +34,15 @@ class FoodData:
 
 class DrinkData:
     """
-    Dataclass to ingest drinks.json and provide the following data members:
+    Dataclass to ingest `drinks.json` and provide the following data members:
     * all_data: raw JSON ingest as a list of dictionaries
     * calorie_lookup: dict of format { drink_name:calories, ...} for
     fast calorie lookup given exact drink name
     """
     def __init__(self):
-        with open('bulk/CalorieData/drinks.json', 'r') as drink_file:
+        # get this file's directory to find drinks.json
+        json_path = os.path.join(os.path.dirname(__file__), 'drinks.json')
+        with open(json_path, 'r') as drink_file:
             self.all_data = loads(drink_file.read())
 
         # create nested dictionary of format above for easy calorie lookup
