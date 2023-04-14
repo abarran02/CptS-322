61c88c82edf776207083277385218cfc98637cda
Alec Barran <alexanderbarran@gmail.com>
2023-04-14 10:11:32 -0700

    Food and Meal (#3)
    * add comprehensive food data
    
    * complete Food and Meal classes with test cases
    
    ---------
    
    Co-authored-by: Matthew Wong <mrw2003x@gmail.com>

---
+++ bulk/posts/models/workout.py
@@ -0,0 +1,13 @@
+from django.db import models
+from .run import Run
+
+class Workout(models.Model):
+    description = models.TextField()
+    run = models.ForeignKey(Run, on_delete=models.CASCADE)
+    calories = models.IntegerField()
+
+    def calories_manual(self, calories: int):
+        self.calories = calories
+
+    def calculate_calories_burned(self, weight: int, metric=False):
+        self.calories = self.run.calculate_calories_burned(weight, metric)
--- bulk/posts/tests.py
+++ bulk/posts/tests.py
@@ -1,3 +1,46 @@
+# https://docs.python.org/3/library/unittest.html
+# refrence to how to use unittest
+import functools
+import os
+import sys
+import unittest
+
 from django.test import TestCase
 
-# Create your tests here.
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
+
+from CalorieData import DrinkData, FoodData
+from posts.models import Meal
+
+class testCases(unittest.TestCase):
+    # test lookup of calorie data from FoodData and DrinkData
+    def test_sum_calories(self):
+        food_data = FoodData().calorie_lookup 
+        drink_data = DrinkData().calorie_lookup
+        
+        meal = []
+        # add two foods and one drink to meal
+        meal.append(food_data["Chick-fil-A"]["Spicy Chicken Sandwich"])
+        meal.append(food_data["Chick-fil-A"]["Large Waffle Fries"])
+        meal.append(drink_data["Sprite"])
+
+        total_calories = functools.reduce(lambda a,b: a + b, meal)
+        self.assertEqual(total_calories,1100)
+
+class ModelMealEntryTestCase(TestCase):
+    def setUp(self):
+        Meal.objects.create(title="test")
+        
+    def test_sum_calories(self):
+        meal = Meal.objects.get(title="test")
+        meal.add_food("Chick-fil-A", "Spicy Chicken Sandwich")
+        meal.add_food("Chick-fil-A", "Large Waffle Fries")
+        meal.add_drink("Sprite")
+        
+        total_calories = meal.sum_calories()
+        self.assertEqual(total_calories,1100)
+    
+if __name__ == "__main__":
+    unittest.main()
