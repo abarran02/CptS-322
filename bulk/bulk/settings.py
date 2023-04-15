645864f7b97baa3c7bfde2614cef03abaad29fab
Alec Barran <alexanderbarran@gmail.com>
2023-04-15 15:39:20 -0700

    move uploads to media path

--- bulk/bulk/settings.py
+++ bulk/bulk/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,3 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "login_redirect"
 LOGOUT_REDIRECT_URL = "home"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
