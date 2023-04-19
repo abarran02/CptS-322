55a5967e0fb45af3ac1427a92c13f9d388ae66d5
Alec Barran <alexanderbarran@gmail.com>
2023-04-18 22:33:05 -0700

    User Profile (#5)
    * update profile model and page
    
    * allow users to follow each other
    
    * update README.md

--- bulk/posts/urls.py
+++ bulk/posts/urls.py
@@ -5,10 +5,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('upload', views.gpx_form_upload, name='upload'),
-    path('posts/<int:post_id>/', views.detail, name='detail')
+    path('', views.home, name='home'),
+    path('posts/<int:post_id>/', views.detail, name='detail'),
+    path('upload/', views.gpx_form_upload, name='upload'),
+    path('users/', views.user_index, name='user_index'),
+    path('users/<int:user_id>/', views.profile, name='profile')
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- bulk/posts/views.py
+++ bulk/posts/views.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
 from accounts.models import UserData
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -10,10 +12,53 @@ from posts.models.run import Run
 from .forms import GPXForm
 
 
-def index(request: HttpRequest):
-    latest_posts = Run.objects.filter(user=request.user).order_by('-pub_date')
-    return render(request, "index.html", {"latest_posts": latest_posts})
+def home(request: HttpRequest):
+    if request.user.is_authenticated:
+        user_data = UserData.objects.get(user=request.user)
+        following = user_data.following.all()
+        latest_posts = Run.objects.filter(user__in=following).order_by('-pub_date')
+    else:
+        latest_posts = []
+    return render(request, "home.html", {"latest_posts": latest_posts})
 
+@login_required
+def user_index(request: HttpRequest):
+    all_users = User.objects.all()
+    return render(request, "user_index.html", {"all_users": all_users})
+
+@login_required
+def profile(request: HttpRequest, user_id):
+    # get target user profile
+    profile = get_object_or_404(User, pk=user_id)
+    
+    following = False
+    user_is_self = profile.id == request.user.id
+    user_data = UserData.objects.get(user=request.user)
+    if not user_is_self:
+        # requesting user follows or unfollows the target profile
+        if 'follow' in request.POST:
+            user_data.following.add(profile)
+
+        if 'unfollow' in request.POST:
+            user_data.following.remove(profile)
+
+        # check whether requesting user is following target profile
+        if not user_data.following.filter(id=profile.id).exists():
+            following = True
+        else:
+            following = False
+    
+    user_entries = Run.objects.filter(user=profile).order_by('-pub_date')
+
+    return render(request, "profile.html", {
+        "profile": profile,
+        "user_data": user_data,
+        "following": following,
+        "user_is_self": user_is_self,
+        "user_entries": user_entries
+    })
+
+@login_required
 def detail(request: HttpRequest, post_id):
     post = get_object_or_404(Run, pk=post_id)
     user_data = UserData.objects.get(user=request.user)
